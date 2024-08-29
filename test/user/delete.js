126d9eca49287137010e9e352c1c8e65f5dd2055
Guanjie Cheng <user.name>
2024-08-29 15:24:08 -0400

    remove unnecessary lines in test file

--- test/user/delete.js
+++ test/user/delete.js
@@ -1,20 +1,12 @@
 'use strict';
 
 const assert = require('assert');
-const path = require('path');
-const fs = require('fs');
-const crypto = require('crypto');
 
-const nconf = require('nconf');
 const db = require('../mocks/databasemock');
 
 const user = require('../../src/user');
-const topics = require('../../src/topics');
-const categories = require('../../src/categories');
-const file = require('../../src/file');
 const utils = require('../../src/utils');
 
-const md5 = filename => crypto.createHash('md5').update(filename).digest('hex');
 
 describe('delete.js', () => {
 	describe('.deleteUserFromFollowers()', () => {
